0485f3e302abad7ca6e8cd362b6cb7bc01ede058
Manjunathan1998 <manjunathsg407@gmail.com>
2025-07-13 01:27:08 +0530

    add

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import time
 import sys 
 
 def main_function(user, phone_number):
+    
     """
     Just a dummy function
     Return string
     """
+
+
     return f"Hello {user} and your phone number is {phone_number}"
-
-
-
 
 
 
