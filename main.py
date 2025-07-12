5d185110b0926585cb33e0cd8502572fe11f02c4
Manjunathan1998 <manjunathsg407@gmail.com>
2025-07-12 23:16:57 +0530

    Hello

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-#Comment new
+"Import nothing"
 def main_function(user, phone_number):
+    """
+    Just a dummy function
+    Return string
+    """
     return f'Hello {user} and your phone number is {phone_number}'
--- main_test.py
+++ main_test.py
@@ -1,5 +1,8 @@
-#Added a new comment a comment
+#No Modules are imported
 from main import main_function
 
 def test_main_function():
+    """
+    Just test function
+    """
     main_function("Manjunathan", 3.14)
