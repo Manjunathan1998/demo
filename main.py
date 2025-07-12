e3782d659333d172851e6e26e13335a780c43846
Manjunathan1998 <manjunathsg407@gmail.com>
2025-07-13 01:37:37 +0530

    add

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ import time
 
 
 def main_function(user, phone_number):
-    
     """
     Just a dummy function
     Return string
     """
-
-
     return f"Hello {user} and your phone number is {phone_number}"
+
+
 
 
 
 
 def function_2():
     pass
+
+
