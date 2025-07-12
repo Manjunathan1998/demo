5ece32a474df4f435a041c2517db7a7882046257
Manjunathan1998 <75794733+Manjunathan1998@users.noreply.github.com>
2025-07-13 01:30:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 "Import modules"
 
 import os
+
 import sys
+
 import time
 
 
 def main_function(user, phone_number):
+    
     """
     Just a dummy function
     Return string
     """
 
+
     return f"Hello {user} and your phone number is {phone_number}"
+
 
 
 def function_2():
